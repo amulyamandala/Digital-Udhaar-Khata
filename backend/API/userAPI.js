371fb89ec87540b5f5b2ca60bd6384bf4cccf0ea
amulyamandala <amulyamandala007@gmail.com>
2026-05-17 16:42:24 +0530

    backend

--- backend/API/userAPI.js
+++ backend/API/userAPI.js
@@ -27,4 +27,25 @@ userApp.get("/profile",verifyToken,async(req,res)=>{
 
 
 })
-//update profile
+//update profile password
+userApp.put("/password",async(req,res)=>{
+    const{email,password,newpassword}=req.body
+    const user=await UserModel.findOne({email})
+    console.log("password:", password);
+    console.log("newpassword:", newpassword);
+    console.log("user.password:", user?.password);
+    if(!user)
+        return res.status(404).json({message:"Email not found"})
+    const isPassValid=await compare(password,user.password)
+    if(!isPassValid)
+        return res.status(401).json({message:"Password Invalid"})
+    const isSame=await compare(newpassword,user.password)
+    if(isSame){
+        return res.status(400).json({message:"The password is same as old"})
+    }
+    const hashed=await hash(newpassword,12)
+    const modified=await UserModel.findOneAndUpdate({email:email},{$set:{password:hashed}},{new:true,runValidators:true})
+    res.status(200).json({message:"Password updated"})
+})
+
+
