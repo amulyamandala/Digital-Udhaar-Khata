df771fdbe0deaecd5090e03b55ed8d09478d85eb
amulyamandala <amulyamandala007@gmail.com>
2026-05-17 16:31:34 +0530

    backend

--- backend/API/userAPI.js
+++ backend/API/userAPI.js
@@ -1,2 +1,8 @@
 import exp from "express";
 import { UserModel } from "../models/userModel";
+import {hash , compare} from "bcrypt"
+import { verifyToken } from "../middleware/verifyToken";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+const{sign,verify}=jwt
+export const userApp=exp.Router()
