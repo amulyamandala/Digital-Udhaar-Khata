27a362b25b672eca69dc5d7f04dbf9b3aa002124
amulyamandala <amulyamandala007@gmail.com>
2026-05-17 16:36:12 +0530

    backend

--- backend/API/userAPI.js
+++ backend/API/userAPI.js
@@ -6,12 +6,16 @@ import { config } from "dotenv";
 import jwt from "jsonwebtoken";
 const{sign,verify}=jwt
 export const userApp=exp.Router()
+//registration
 userApp.post("/register",async(req,res,next)=>{
 
 })
+//login
 userApp.post("/login",async(req,res)=>{
 
 })
+//logout 
 userApp.get("/logout",(req,res)=>{
-    
+
 })
+//check-auth
