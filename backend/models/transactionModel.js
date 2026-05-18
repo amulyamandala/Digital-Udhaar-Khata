90bf685b24f47d6cd76debd4b089545cde17bf7e
amulyamandala <amulyamandala007@gmail.com>
2026-05-18 15:18:23 +0530

    backend

--- backend/models/transactionModel.js
+++ backend/models/transactionModel.js
@@ -1,5 +1,29 @@
 import { Schema, model } from "mongoose";
 const transactionSchema=new Schema({
+  customerId:{
+    type:Schema.Types.ObjectId
+  },
+  shopId:{
+    type:Schema.Types.ObjectId
+  },
+  type:{
+
+  }, // CREDIT or DEBIT
+  amount:{
+
+  },
+  description:{
+
+  },
+  paymentMethod:{
+
+  },
+  createdBy:{
+
+  },
+  createdAt:{
+    
+  }
 
 })
-export const tr
+export const TransactionModel=model("transaction",transactionSchema)
